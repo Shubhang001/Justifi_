6abd87fca950ad55847a6c16b77f0ce5f5c7e7b1
Mitun Suresh <mitun.focused@gmail.com>
2023-06-06 22:37:11 +0530

    get subcategory update

--- lib/api/subcategory/get_subcategory.dart
+++ lib/api/subcategory/get_subcategory.dart
@@ -1,1 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:jusitfi_admin/api/base_url.dart';
 
+getSubcategory(int categoryType) async {
+  final response = await http
+      .get(Uri.parse("$baseURL/v1/categories/$categoryType/sub_categories/"));
+
+  if (response.statusCode == 200) {
+    return SubCategoryModel.fromJson(jsonDecode(response.body));
+  } else {
+    return "Failed";
+  }
+}
+
+class SubCategoryModel {
+  final int count;
+  final bool next;
+  final bool previous;
+  final List results;
+
+  const SubCategoryModel({
+    required this.count,
+    required this.next,
+    required this.previous,
+    required this.results,
+  });
+
+  factory SubCategoryModel.fromJson(Map<String, dynamic> json) {
+    return SubCategoryModel(
+      count: json['id'],
+      next: json['next'],
+      previous: json['previous'],
+      results: json['results'],
+    );
+  }
+}
