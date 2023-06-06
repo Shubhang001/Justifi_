d18ac9a2595435bae478624e99f2d6dd91555af0
Mitun Suresh <mitun.focused@gmail.com>
2023-06-06 22:58:54 +0530

    api update

--- lib/api/subcategory/add_subcategory.dart
+++ lib/api/subcategory/add_subcategory.dart
@@ -1,23 +1,28 @@
 import 'dart:convert';
-
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:jusitfi_admin/api/base_url.dart';
 
-Future<String> addCategory(String categoryName, categoryCode, logoPath) async {
-  var bytes = await rootBundle.load(logoPath);
+// v1/categories/:category/sub_categories/
+
+Future<String> addSubCategory(
+  String categoryName,
+  subcategoryCode,
+  subcategoryLogo,
+) async {
+  var bytes = await rootBundle.load(subcategoryLogo);
   var buffer = bytes.buffer;
   var imageBytes = buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
   var base64Image = base64Encode(imageBytes);
 
   final response = await http.post(
-    Uri.parse("$baseURL/v1/categories/"),
+    Uri.parse("$baseURL/v1/categories/$categoryName/sub_categories/"),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
     body: jsonEncode(<String, String>{
       'name': categoryName,
-      'code': categoryCode,
+      'code': subcategoryCode,
       'logo': base64Image,
     }),
   );
