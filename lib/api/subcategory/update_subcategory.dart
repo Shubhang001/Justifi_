6f864dea68ef21af481624b7af79ca2d1ca7288b
Mitun Suresh <mitun.focused@gmail.com>
2023-06-06 23:25:44 +0530

    patch & delete update

--- lib/api/subcategory/update_subcategory.dart
+++ lib/api/subcategory/update_subcategory.dart
@@ -1,16 +1,21 @@
 import 'package:http/http.dart' as http;
 import 'package:jusitfi_admin/api/base_url.dart';
 
+// {{base_url}}/v1/categories/:category/sub_categories/:sub_category
+
 Future<String> updateCategories(
-  int categoryType,
-  bool isActive,
+  String categoryId,
+  subCategoryId,
+  isActive,
+  description,
 ) async {
   final response = await http.patch(
     body: {
       "is_active": isActive,
+      "description": description,
     },
     Uri.parse(
-      "$baseURL/v1/categories/$categoryType/",
+      "$baseURL/v1/categories/$subCategoryId/",
     ),
   );
 
