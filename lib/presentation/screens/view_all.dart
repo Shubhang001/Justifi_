8b419529ad6be2be859431f92a7c0f778d323fc8
nived-s <nivedsreenilayam@gmail.com>
2023-10-15 11:15:46 +0530

    added lawyer profile page

--- lib/presentation/screens/view_all.dart
+++ lib/presentation/screens/view_all.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:jusitfi_admin/presentation/screens/lawyer_profile_temperory.dart';
 import 'package:jusitfi_admin/presentation/widgets/advocate_card_extended.dart';
 import 'package:jusitfi_admin/presentation/widgets/filter_sort.dart';
 import 'package:jusitfi_admin/presentation/widgets/searchbar.dart';
@@ -81,173 +82,184 @@ class _ViewAllPageState extends State<ViewAllPage> {
                     child: ListView.builder(
                       itemCount: 5,
                       itemBuilder: (context, index) {
-                        return Container(
-                          margin: EdgeInsets.only(bottom:8),
-                          height: 300,
-                          width: 300,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white, width: 2.0),
-                            color: Colors.black,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  CircleAvatar(
-                                    radius: 75.0,
-                                    backgroundColor: Colors.transparent,
-                                    child: ClipOval(
-                                      child: Image.asset(
-                                        'assets/images/advocate_img.png',
-                                        width: 120,
-                                        height: 120,
-                                      ),
-                                    ),
-                                  ),
-                                  CircleAvatar(
-                                    radius: 60.0,
-                                    backgroundColor: Colors.transparent,
-                                    child: ClipOval(
-                                      child: Image.asset(
-                                        "assets/images/justify_logo.png",
-                                        width: 130,
-                                        height: 130,
-                                      ),
-                                    ),
-                                  ),
-                                ],
+                        return GestureDetector(
+                          onTap: (() {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LawyerProfileScreen(),
                               ),
-                              Container(
-                                color: Colors.white,
-                                child: Column(
+                            );
+                          }),
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 8),
+                            height: 300,
+                            width: 300,
+                            decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.white, width: 2.0),
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
                                   children: [
-                                    Container(
-                                      color: Colors.white,
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceAround,
-                                        children: [
-                                          Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                'Sakshi',
-                                                style: kAdvocateCardName,
-                                              ),
-                                              // Row(
-                                              //   children: [
-                                              //     const Icon(
-                                              //       Icons.pin_drop,
-                                              //       color: Colors.red,
-                                              //       size: 10,
-                                              //     ),
-                                              //     Text(
-                                              //       '5 kms from your location',
-                                              //       style:
-                                              //           kAdvocateCardLocation,
-                                              //     )
-                                              //   ],
-                                              // ),
-                                            ],
-                                          ),
-                                          Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceAround,
-                                            children: [
-                                              Text(
-                                                '4',
-                                                style: kAdvocateCardRating,
-                                              ),
-                                              StarRating(rating: 4)
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                    Container(
-                                      color: Colors.white,
-                                      child: IntrinsicHeight(
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceAround,
-                                          children: [
-                                            Column(
-                                              children: [
-                                                Text(
-                                                  '13',
-                                                  style: kAdvocateCardCount,
-                                                ),
-                                                Text(
-                                                  'Clients',
-                                                  style:
-                                                      kAdvocateCardCountTitle,
-                                                )
-                                              ],
-                                            ),
-                                            const VerticalDivider(
-                                              color: Colors.black,
-                                            ),
-                                            Column(
-                                              children: [
-                                                Text(
-                                                  '10',
-                                                  style: kAdvocateCardCount,
-                                                ),
-                                                Text(
-                                                  'Cases',
-                                                  style:
-                                                      kAdvocateCardCountTitle,
-                                                )
-                                              ],
-                                            ),
-                                            const VerticalDivider(
-                                              color: Colors.black,
-                                            ),
-                                            Column(
-                                              children: [
-                                                Text(
-                                                  '4',
-                                                  style: kAdvocateCardCount,
-                                                ),
-                                                Text(
-                                                  'Experiences',
-                                                  style:
-                                                      kAdvocateCardCountTitle,
-                                                )
-                                              ],
-                                            )
-                                          ],
+                                    CircleAvatar(
+                                      radius: 75.0,
+                                      backgroundColor: Colors.transparent,
+                                      child: ClipOval(
+                                        child: Image.asset(
+                                          'assets/images/advocate_img.png',
+                                          width: 120,
+                                          height: 120,
                                         ),
                                       ),
                                     ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Align(
-                                        alignment: Alignment.bottomRight,
-                                        child: Container(
-                                          height: 20,
-                                          width: 80,
-                                          decoration: BoxDecoration(
-                                              color: Colors.green,
-                                              borderRadius:
-                                                  BorderRadius.circular(2)),
-                                          child: Center(
-                                            child: Text(
-                                              'View Profile',
-                                              style: kProfileButtonText,
-                                            ),
-                                          ),
+                                    CircleAvatar(
+                                      radius: 60.0,
+                                      backgroundColor: Colors.transparent,
+                                      child: ClipOval(
+                                        child: Image.asset(
+                                          "assets/images/justify_logo.png",
+                                          width: 130,
+                                          height: 130,
                                         ),
                                       ),
                                     ),
                                   ],
                                 ),
-                              ),
-                            ],
+                                Container(
+                                  color: Colors.white,
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        color: Colors.white,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  'Sakshi',
+                                                  style: kAdvocateCardName,
+                                                ),
+                                                // Row(
+                                                //   children: [
+                                                //     const Icon(
+                                                //       Icons.pin_drop,
+                                                //       color: Colors.red,
+                                                //       size: 10,
+                                                //     ),
+                                                //     Text(
+                                                //       '5 kms from your location',
+                                                //       style:
+                                                //           kAdvocateCardLocation,
+                                                //     )
+                                                //   ],
+                                                // ),
+                                              ],
+                                            ),
+                                            Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceAround,
+                                              children: [
+                                                Text(
+                                                  '4',
+                                                  style: kAdvocateCardRating,
+                                                ),
+                                                StarRating(rating: 4)
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      Container(
+                                        color: Colors.white,
+                                        child: IntrinsicHeight(
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceAround,
+                                            children: [
+                                              Column(
+                                                children: [
+                                                  Text(
+                                                    '13',
+                                                    style: kAdvocateCardCount,
+                                                  ),
+                                                  Text(
+                                                    'Clients',
+                                                    style:
+                                                        kAdvocateCardCountTitle,
+                                                  )
+                                                ],
+                                              ),
+                                              const VerticalDivider(
+                                                color: Colors.black,
+                                              ),
+                                              Column(
+                                                children: [
+                                                  Text(
+                                                    '10',
+                                                    style: kAdvocateCardCount,
+                                                  ),
+                                                  Text(
+                                                    'Cases',
+                                                    style:
+                                                        kAdvocateCardCountTitle,
+                                                  )
+                                                ],
+                                              ),
+                                              const VerticalDivider(
+                                                color: Colors.black,
+                                              ),
+                                              Column(
+                                                children: [
+                                                  Text(
+                                                    '4',
+                                                    style: kAdvocateCardCount,
+                                                  ),
+                                                  Text(
+                                                    'Experiences',
+                                                    style:
+                                                        kAdvocateCardCountTitle,
+                                                  )
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Align(
+                                          alignment: Alignment.bottomRight,
+                                          child: Container(
+                                            height: 20,
+                                            width: 80,
+                                            decoration: BoxDecoration(
+                                                color: Colors.green,
+                                                borderRadius:
+                                                    BorderRadius.circular(2)),
+                                            child: Center(
+                                              child: Text(
+                                                'View Profile',
+                                                style: kProfileButtonText,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         );
                       },
